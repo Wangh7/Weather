61802521332c8393164cf152f44fc6021cdb874d
wangh7 <978102365@qq.com>
2018-12-05 14:32:05 +0800

    完成了 GSON实体类的定义

---
+++ app/src/main/java/com/example/wangh7/weather/Gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.wangh7.weather.Gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
